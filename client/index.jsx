788c9a7810041ad285b2886be6fc1fbec460b327
jmazrimas <joe.mazrimas@gmail.com>
2017-10-14 16:43:02 -0500

    nav elements abstracted in to react class

--- client/index.jsx
+++ client/index.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { NavBar } from 'navbar';
 
+let navs = [
+  'View One',
+  'View Two',
+  'View Three'
+]
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -11,7 +17,7 @@ class App extends React.Component {
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <NavBar />
+                    <NavBar navs={navs} />
                     <div className="col-lg-10 col-md-10 col-sm-10">
                         <h2>Content</h2>
                     </div>
--- client/navbar.jsx
+++ client/navbar.jsx
@@ -10,11 +10,25 @@ export class NavBar extends React.Component {
       <div className="col-lg-2 col-md-2 col-sm-2">
         <h2>Nav Bar</h2>
         <ul className="list-group">
-          <li className="list-group-item">View 1</li>
-          <li className="list-group-item">View 2</li>
-          <li className="list-group-item active">View 3</li>
+          {
+            this.props.navs.map((nav) => {
+              return <NavItem key={nav} navName={nav}/>
+            })
+          }
         </ul>
       </div>
+    )
+  }
+}
+
+class NavItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      // need to add the active class
+      <li className="list-group-item">{this.props.navName}</li>
     )
   }
 }
